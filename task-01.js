2fcd14a280bcf282a663b4e3b90fd07709ede989
Kartavchenko <stranger1301078@gmail.com>
2022-06-10 19:41:40 +0200

    task-2

--- task-01.js
+++ task-01.js
@@ -4,15 +4,31 @@
 //Удалите первый элемент массива и выведите его в консоль.
 //Вставьте «Рэп» и «Регги» в начало массива
 
-const styles = ['Джаз', 'Блюз'];
-styles.push('Рок-н-ролл');
+// const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
 
-const indexBluz = styles.indexOf('Блюз');
+// const indexBluz = styles.indexOf('Блюз');
 
-styles.splice(indexBluz, 1, 'Классика');
+// styles.splice(indexBluz, 1, 'Классика');
 
-console.log(styles.shift());
+// console.log(styles.shift());
 
-styles.unshift('Рэп', 'Регги');
+// styles.unshift('Рэп', 'Регги');
 
-console.log(styles);
+// console.log(styles);
+
+//Напишите функцию min(a, b), которая возвращает
+//меньшее из чисел a, b
+//нужно добавить проверку, что функция получает числа
+
+
+// function min(a, b) {
+//     return a < b ? a : b;
+//     // if (a < b) {
+//     //     console.log(a)
+//     // } else {
+//     //     console.log(b)
+//     // }
+//     // console.log(a, b)
+// }
+// console.log(min(5, 8))
